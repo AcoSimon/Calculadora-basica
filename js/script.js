015b1cb7401220f282a03996147bd5bc0cc63a05
Acosta Simon <108154682+AcoSimon@users.noreply.github.com>
2022-08-07 14:21:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,106 +1,83 @@
-console.log('Calculadora:');
-
-
-// Panel numerico:
-let e0 = document.getElementById('0');
-let e1 = document.getElementById('1').value;
-let e2 = document.getElementById('2').value;
-let e3 = document.getElementById('3').value;
-let e4 = document.getElementById('4').value;
-let e5 = document.getElementById('5').value;
-let e6 = document.getElementById('6').value;
-let e7 = document.getElementById('7').value;
-let e8 = document.getElementById('8').value;
-let e9 = document.getElementById('9').value;
-
+console.log('Calculadora: 07/08/2022');
+let resultados = [];
+let estado = 1;
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Funciones principales:
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+function extraerDato(e){
+    // Extraer dato:
 /*
-git pull
-git status
-git add .
-git commit -m "mensajiño"
- */
+    Signatura de la funcion:
+                            String -> None
+    Funcion:
+            Extrae el valor que posea el elemento HTML, en este caso el <input id=screen>, y lo suma al Input-text que se utiliza como display.
+            Su lugar de operacion es dentro del formulario "Calculadora."
+*/
 
-// Funciones:
-function suma(x, y) {
-    let resultado = parseFloat(x) + parseFloat(y);
-    return resultado;
+
+    document.calculadora.display.value+=e;
 }
+//------------------------------------------------------------------------
+function realizarOperacion() {
+    // Entregar historial:
+/*
+    Signatura de la funcion:
+                            
+    Funcion:
 
-function resta(x, y) {
-    let resultado = parseFloat(x) - parseFloat(y);
-    return resultado;
+*/
+    document.calculadora.display.value=eval(document.calculadora.display.value);
+    // Almacenador: variable global.
+    resultados.push(document.calculadora.display.value);
+    console.log(resultados);
 }
-
-function multiplicacion(x, y) {
-    let resultado = parseFloat(x) * parseFloat(y);
-    return resultado;
+//------------------------------------------------------------------------
+function historial() {
+  let html = ``;
+    for (let i = 0; i < resultados.length; i++) {
+        html += resultados[i]+` ; `;
+    }
+    alert(html);
 }
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Funciones auxiliares:
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+function invocarpi(e){
+        // Entregar pi:
+/*
+    Signatura de la funcion:
+                            number->none
+    Funcion:
+            Imprime en pantalla el numero aproximado a π. (Reemplaza lo ingresado)
+*/
 
-function division(x, y) {
-    let resultado = parseFloat(x) / parseFloat(y);
-    return resultado;
+    document.calculadora.display.value=eval(3.14159265359);
+
+    // Almacenador: variable global.
+    resultados.push(document.calculadora.display.value);
+    console.log(resultados);
 }
 
 function raiz() {
-    var x = document.getElementById('numero')
-    console.log(x)
-    let resultado = Math.sqrt(x);
-    numero.innerHTML = resultado;    
-}
+            // Entregar raiz cuadrada: (solo base 2)
+/*
+    Signatura de la funcion:
+                            number->none
+    Funcion:
+            Imprime en pantalla el numero aproximado a π. (Reemplaza lo ingresado)
+*/
+    let raiz = Math.sqrt(parseFloat(document.calculadora.display.value));
 
-function potencia(x) {
-    let resultado = parseFloat(x)*parseFloat(x);
-    return  resultado;
+    document.calculadora.display.value=eval(raiz);
+    // Almacenador: variable global.
+    resultados.push(document.calculadora.display.value);
+    console.log(resultados);
 }
-
-function porcentaje(x, porcentaje) {
-    let multiplicacion = parseFloat(x) * porcentaje;
-    let dividir = multiplicacion / 100;
-    return dividir;
-}
-
-function modulo(x, y) {
-    let modulo = parseFloat(x) % parseFloat(y) ;
-    return resultado;
-}
-
-function escribir(x) {
-  let dato = document.getElementById(x);
-  console.log(dato);
-  numero.innerHTMLL += dato
-}
-
-
-function escribir0(){
-  numero.innerHTML += '0'
-}
-function escribir1(){
-  numero.innerHTML += '1'
-}
-function escribir2(){
-  numero.innerHTML += '2'
-}
-function escribir3(){
-  numero.innerHTML += '3'
-}
-function escribir4(){
-  numero.innerHTML += '4'
-}
-function escribir5(){
-  numero.innerHTML += '5'
-}
-function escribir6(){
-  numero.innerHTML += '6'
-}
-function escribir7(){
-  numero.innerHTML += '7'
-}
-function escribir8(){
-  numero.innerHTML += '8'
-}
-function escribir9(){
-  numero.innerHTML += '9'
-}
-function borrar(){
-  numero.innerHTML = ''
+function potencia() {
+    let potencia2 = parseFloat(document.calculadora.display.value)*parseFloat(document.calculadora.display.value); 
+    document.calculadora.display.value=eval(potencia2);
+    
+    // Almacenador: variable global.
+    resultados.push(document.calculadora.display.value);
+    console.log(resultados);
 }
